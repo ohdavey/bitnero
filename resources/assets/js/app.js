fefe774e6bfc21df8d72e368835f2f625adb10b3
Davey Contreras <dcontreras52@gmail.com>
2018-01-16 22:49:06 -0500

    Introduce new vue.js component for crypto content.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,7 +8,6 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -16,6 +15,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('crypto', require('./components/Crypto.vue'));
 
 const app = new Vue({
     el: '#app'
